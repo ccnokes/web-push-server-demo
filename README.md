c12e1b236e52b391a66f94e3972f213c471f290c
Cameron Nokes <ccnokes@gmail.com>
2018-10-01 16:53:34 -0600

    update endpoints with better handling

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # web push backend demo
-This is a web server that provides a minimal server implementation to get web push working.
+This is a web server that provides a minimal server implementation to get web push working. It's useful for prototyping or as a simple reference guide to know how to start implementing a backend for web push.
 
 ## Starting
 ```
@@ -8,4 +8,4 @@ npm start
 ```
 Running npm install will also generate VAPID server keys that you will need to send requests.
 
-See `register-service-worker-sample.js` to see how to use the server.
+See `register-service-worker-sample.js` to see how to set it up in the browser.
--- index.js
+++ index.js
@@ -42,14 +42,39 @@ fastify.post(
   },
   async (request, reply) => {
     fastify.log.info(`register push subscription for user ${request.body.userId}`);
+
+    let now = Date.now();
     let document = Object.assign(request.body, {
-      timestamp: Date.now()
+      created: now,
+      lastModified: now,
+      service: new URL(request.body.endpoint).host
     });
 
-    let docs = await find({ userId: request.body.userId, endpoint: request.body.endpoint });
-    if (docs.length === 0) {
+    // check if this subscription is already registered
+    let existingSub = await findOne({
+      userId: request.body.userId,
+      endpoint: request.body.endpoint
+    });
+    if (!existingSub) {
       fastify.log.info(`no subscription found, creating`);
       await insert(document);
+    } else {
+      fastify.log.info(`subscription found, updating`);
+      await update({ _id: existingSub._id }, { $set: { lastModified: Date.now() } });
+    }
+
+    // check for old subscriptions from the same service as this one that we can drop
+    let oldSubs = await find({
+      userId: request.body.userId,
+      service: document.service,
+      endpoint: { $ne: request.body.endpoint }
+    });
+
+    if (oldSubs.length > 0) {
+      fastify.log.info(`dropping ${oldSubs.length} old subscription(s)`);
+      await Promise.all(oldSubs.map(sub => {
+        return remove({ _id: sub._id });
+      }));
     }
 
     reply.code(200).send('');
@@ -92,15 +117,34 @@ fastify.post(
   async (request, reply) => {
     try {
       let subs = await find({ userId: request.body.userId });
-      await Promise.all(subs.map(sub => {
-        webpush.sendNotification({
+      await Promise.all(subs.map(async sub => {
+
+        let subscription = {
           endpoint: sub.endpoint,
           keys: sub.keys
-        }, JSON.stringify(request.body.notification));
+        };
+        let payload = JSON.stringify(request.body.notification);
+        let options = {
+          TTL: 60 * 60 * 24 * 30 // lives for 30 days
+        };
+        try {
+          await webpush.sendNotification(subscription, payload, options);
+        }
+        catch (error) {
+          // a push service will return a 410 if a subscription has been deactivated
+          // See https://developers.google.com/web/fundamentals/push-notifications/common-issues-and-reporting-bugs#http_status_codes
+          if (error.statusCode === 410) {
+            fastify.log.info(`Removing de-registered subscription`);
+            await remove({ _id: sub._id });
+          } else {
+            fastify.log.error(error);
+          }
+        }
+
       }));
       reply.code(200).send(`Sent ${subs.length} messages`);
     }
-    catch(error) {
+    catch (error) {
       fastify.log.error(error);
       reply.code(500).send(error.message);
     }
